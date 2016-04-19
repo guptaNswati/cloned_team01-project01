dcf398b760e4321ff2725967b985b9e1b56b3323
TerryTsao <xxc142@case.edu>
2016-04-19 00:32:55 -0700

    a class that contains celestial info

---
+++ src/gamerules/CelestialInfo.java
@@ -0,0 +1,23 @@
+package gamerules;
+
+public class CelestialInfo
+{
+   private double size;
+   private double distanceFromSun;
+   private String composition;
+   private String originOfName;
+   private String funFact;
+
+   // getters and setters if needed
+
+   @Override
+   public String toString()
+   {
+      return "";
+   }
+
+   public void showInfo()
+   {
+
+   }
+}
