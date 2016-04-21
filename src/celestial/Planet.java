4aaf85cd9816c864a436ba91347f477ada26671c
TerryTsao <xxc142@case.edu>
2016-04-21 01:55:52 -0700

    change implementation of planet

--- src/celestial/Planet.java
+++ src/celestial/Planet.java
@@ -4,12 +4,31 @@ import java.awt.Color;
 import java.awt.Point;
 
 public class Planet extends Celestial {
-   public Planet() {
-      super();
+   private final int distanceToSun;
+   private final int periodInMS;
+   private double angleToSun;
+
+   public Planet(Color color, String name, double radius, int distanceToSun,
+         double angleToSun, int periodInMS) {
+      super(new Point(), color, name, radius);
+      this.distanceToSun = distanceToSun > 0 ? distanceToSun : 10;
+      this.setAngleToSun(angleToSun);
+      this.periodInMS = periodInMS > 1000 ? periodInMS : 1000;
    }
 
-   public Planet(Point coordinate, Color color, String name, double radius) {
-      super(coordinate, color, name, radius);
+   public int getDistanceToSun() {
+      return distanceToSun;
    }
 
+   public double getAngleToSun() {
+      return angleToSun;
+   }
+
+   public void setAngleToSun(double angleToSun) {
+      this.angleToSun = angleToSun;
+   }
+
+   public int getPeriodInMS() {
+      return periodInMS;
+   }
 }
