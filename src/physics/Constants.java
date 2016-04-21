026071fc74e64472175c869ff3a8814db4f4ca31
TerryTsao <xxc142@case.edu>
2016-04-21 02:32:37 -0700

    added frame boundaries to constants

--- src/physics/Constants.java
+++ src/physics/Constants.java
@@ -2,4 +2,7 @@ package physics;
 
 public class Constants {
    public static final int TIME_INTERVAL = 40;
+
+   public static final int FRAME_WIDTH = 1700;
+   public static final int FRAME_HEIGHT = 1000;
 }
