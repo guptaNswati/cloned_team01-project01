9963af194df9c031d66d82670626361570b29a69
TerryTsao <xxc142@case.edu>
2016-04-20 23:06:18 -0700

    added celestial

---
+++ src/celestial/Celetial.java
@@ -0,0 +1,13 @@
+package celestial;
+
+import java.awt.Color;
+import java.awt.Point;
+
+public abstract class Celetial
+{
+   private Point coordinate;
+   private Color color;
+   private String name;
+
+   public abstract void draw();
+}
