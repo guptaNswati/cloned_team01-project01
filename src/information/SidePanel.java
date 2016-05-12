650068833eddd29a3cc8597e3fdd78c3adabb4c7
TerryTsao <xxc142@case.edu>
2016-05-12 01:19:21 -0700

    added textArea in class SidePanel

--- src/information/SidePanel.java
+++ src/information/SidePanel.java
@@ -3,12 +3,14 @@ package information;
 import java.awt.BorderLayout;
 
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import ship.Ship;
 
 public class SidePanel extends JPanel {
    private JTextField thrustInput;
+   private JTextArea jokeTextBox, infoTextBox;
 
    public SidePanel(Ship ship) {
       setLayout(new BorderLayout());
@@ -29,5 +31,35 @@ public class SidePanel extends JPanel {
     */
    public void setThrustInput(JTextField thrustInput) {
       this.thrustInput = thrustInput;
+   }
+
+   /**
+    * @return the jokeTextBox
+    */
+   public JTextArea getJokeTextBox() {
+      return jokeTextBox;
+   }
+
+   /**
+    * @param jokeTextBox
+    *           the jokeTextBox to set
+    */
+   public void setJokeTextBox(JTextArea jokeTextBox) {
+      this.jokeTextBox = jokeTextBox;
+   }
+
+   /**
+    * @return the infoTextBox
+    */
+   public JTextArea getInfoTextBox() {
+      return infoTextBox;
+   }
+
+   /**
+    * @param infoTextBox
+    *           the infoTextBox to set
+    */
+   public void setInfoTextBox(JTextArea infoTextBox) {
+      this.infoTextBox = infoTextBox;
    }
 }
