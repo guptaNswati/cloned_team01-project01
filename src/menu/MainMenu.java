a74f321b34ddfb7775bd062f584c28fb2e08ae55
TerryTsao <xxc142@case.edu>
2016-05-14 22:14:35 -0700

    remove extra main

--- src/menu/MainMenu.java
+++ src/menu/MainMenu.java
@@ -122,8 +122,4 @@ public class MainMenu implements ActionListener {
          helpPanel.setVisible(false);
       }
    }
-
-   public static void main(String[] args) {
-      MainMenu menu = new MainMenu();
-   }
 }
